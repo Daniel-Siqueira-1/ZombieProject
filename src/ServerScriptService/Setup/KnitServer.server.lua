23190f92a06692cc5259f9b5fa22294606464c76
Daniel-Siqueira-1 <77032032+Daniel-Siqueira-1@users.noreply.github.com>
2023-04-25 21:31:10 -0300

    Update KnitServer.server.lua

--- src/ServerScriptService/Setup/KnitServer.server.lua
+++ src/ServerScriptService/Setup/KnitServer.server.lua
@@ -1,7 +1,7 @@
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
 local ServerStorage = game:GetService("ServerStorage")
 local Knit = require(ReplicatedStorage.Packages.Knit)
-
+print('TEST')
 for _,Module in pairs(ServerStorage.Services:GetDescendants()) do 
     if Module:IsA("ModuleScript") and Module.Name:match("Service$") then
         require(Module)
